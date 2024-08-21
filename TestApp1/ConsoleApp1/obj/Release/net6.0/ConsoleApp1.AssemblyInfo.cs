504c367cb9130da4f4ecea3c0ccc4ae5331282d7
hubgitjohann <johannogbae02@gmail.com>
2024-08-21 16:20:59 +1000

    update

--- TestApp1/ConsoleApp1/obj/Release/net6.0/ConsoleApp1.AssemblyInfo.cs
+++ TestApp1/ConsoleApp1/obj/Release/net6.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56883df128d56b4e6bf5db4d7a36a9c6eda2ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
